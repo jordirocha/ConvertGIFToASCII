4e3babdf475bc328d8a7becad45fa65bf2becb8d
jordi <jordy_gta2000a.r@hotmail.com>
2021-08-31 13:33:55 +0200

    Script to install dependencies

---
+++ dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $(whoami) == root ]; then
+  python3 -m pip install PySimpleGUI
+  echo 'deb [trusted=yes] https://apt.fury.io/ascii-image-converter/ /' | sudo tee /etc/apt/sources.list.d/ascii-image-converter.list
+  apt update
+  apt install -y ascii-image-converter
+
+  apt install -y ffmpeg
+
+  echo "All okay!"
+else
+  echo Please run this script as root or using sudo
+  exit 1
+fi
