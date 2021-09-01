111a29b01ed4b96712dea2b1d74be90b3867118f
jordi <jordy_gta2000a.r@hotmail.com>
2021-09-01 09:07:58 +0200

    Changes on result script

--- scripts/resultToGif.sh
+++ scripts/resultToGif.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DISTRO=$(cat /etc/issue.net | grep -i -E '[kde|ubuntu]')
+DISTRO=$(cat /etc/issue.net | grep -i -E '[kde|ubuntu|debian]')
 
 case $DISTRO in
 *KDE*)
   # add --fullscren after first parameter to open it on fullscren
   konsole --new-tab -p tabtitle="Result to GIF" -e "$SHELL" -c "sh ./scripts/convertToASCII.sh; rm -r ./scripts/images; sleep 1"
   ;;
-*Ubuntu*)
-  gnome-terminal -- /bin/sh -c 'sh ./scripts/convertToASCII.sh; rm -r ./scripts/images; sleep 1'
+*Ubuntu*|*Debian*)
+  gnome-terminal --maximize -- /bin/sh -c 'sh ./scripts/convertToASCII.sh; rm -r ./scripts/images; sleep 1'
   ;;
 *)
   echo "Not compatible :(."
